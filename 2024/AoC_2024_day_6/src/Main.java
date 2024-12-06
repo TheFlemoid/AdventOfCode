8af1cd2d883eca728f453b2358cafbdc1054e44b
TheFlemoid <trey.dahlberg@gmail.com>
2024-12-06 09:15:18 -0600

    Initial commit

---
+++ 2024/AoC_2024_day_6/src/Main.java
@@ -0,0 +1,27 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("The puzzle floor plan must be passed as a param. Exiting.");
+            System.exit(1);
+        }
+
+        Path floorPlanPath = Paths.get(args[0]);
+        String floorPlan = "";
+
+        try {
+            floorPlan = Files.readString(floorPlanPath);
+        } catch(IOException e) {
+            System.out.println("IOException when attempting to read " + args[0] + e.getMessage());
+            System.out.println("Exiting");
+            System.exit(1);
+        }
+
+        System.out.println();
+        System.out.printf(floorPlan);
+    }
+}
